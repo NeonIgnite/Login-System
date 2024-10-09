136298e10304e958b1e4651c9a70a100f30c470d
NeonIgnite <khaledashraf310@gmail.com>
2024-10-09 11:01:53 +0200

    First Commit uploading file

---
+++ registerSystem/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Register from './components/Register'
+import Login from './components/Login'
+
+
+function App() {
+  return (
+    <>
+      <Register></Register>
+      <Login></Login>
+    </>
+  )
+}
+
+export default App
